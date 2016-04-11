3c4328383cda2b030aefb12b71c70a2df883d870
qnnnnez <qnnnnez@live.com>
2016-04-11 01:54:12 +0000

    tolerate cookies formatted in js string

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import ehentai
 import gallery
 import os
 import copy
+import json
 
 
 def main():
@@ -15,11 +16,14 @@ def main():
     else:
         cookiejar = ehentai.GreatCookieJar()
         cookies = input('document.cookie: ')
+        if cookies.startswith('\"') and cookies.endswith('\"'):
+            cookies = json.loads(cookies)
         cookies = ehentai.convert_cookies(cookies)
         cookiejar.update(cookies)
+        open(cookie_file_path, 'w').write(repr(cookiejar))
 
     loop = asyncio.get_event_loop()
-    #loop.set_debug(True)
+    loop.set_debug(True)
 
     conn = aiohttp.TCPConnector(limit=20)
     with aiohttp.ClientSession(loop=loop, connector=conn, cookies=cookiejar.get_dict()) as session:
@@ -31,3 +35,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
